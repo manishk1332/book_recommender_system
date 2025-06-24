12ef7a95a9cb4aca6bc0fa024d741fe8addbdb99
Manish Kundu <116656965+manishk1332@users.noreply.github.com>
2025-06-25 00:46:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+import gradio as gr
+
+load_dotenv()
+
+books = pd.read_csv("books_with_emotions.csv")
+books["img"] = np.where(
+    books["img"].isna(),
+    "cover-not-found.jpg",
+    books["img"],
+)
+
+hf_embedding = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+PERSIST_DIRECTORY = "./chroma_db_books"
+db_books = Chroma(
+    persist_directory=PERSIST_DIRECTORY,
+    embedding_function=hf_embedding
+)
+
+
+def retrieve_semantic_recommendations(
+        query: str,
+        category: str = None,
+        tone: str = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
+) -> pd.DataFrame:
+
+    recs = db_books.similarity_search(query, k=initial_top_k)
+    books_list = [rec.page_content.strip('"').split()[0] for rec in recs]
+    book_recs = books[books["isbn"].isin(books_list)].head(initial_top_k)
+
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_categories"] == category].head(final_top_k)
+    else:
+        book_recs = book_recs.head(final_top_k)
+
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="anger", ascending=False, inplace=True)
+    elif tone == "Suspenseful":
+        book_recs.sort_values(by="fear", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="sadness", ascending=False, inplace=True)
+
+    return book_recs
+
+
+def recommend_books(
+        query: str,
+        category: str,
+        tone: str
+):
+    recommendations = retrieve_semantic_recommendations(query, category, tone)
+    results = []
+
+    for _, row in recommendations.iterrows():
+        description = row["desc"]
+        truncated_desc_split = description.split()
+        truncated_description = " ".join(truncated_desc_split[:30]) + "..."
+
+        authors_split = row["author"].split(";")
+        if len(authors_split) == 2:
+            authors_str = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])}, and {authors_split[-1]}"
+        else:
+            authors_str = row["author"]
+
+        caption = f"{row['title']} by {authors_str}: {truncated_description}"
+        results.append((row["img"], caption))
+    return results
+
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
+        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        submit_button = gr.Button("Find recommendations")
+
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
+
+    submit_button.click(fn = recommend_books,
+                        inputs = [user_query, category_dropdown, tone_dropdown],
+                        outputs = output)
+
+
+if __name__ == "__main__":
+    dashboard.launch()
